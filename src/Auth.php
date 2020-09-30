871410780c9ed114f9fd44202e3f65b5e229136c
Diorges Rocha <diorgesl@users.noreply.github.com>
2020-09-30 20:18:58 +0000

    Apply fixes from StyleCI

--- src/Auth.php
+++ src/Auth.php
@@ -2,32 +2,31 @@
 
 namespace Diorgesl\DiorgesBB;
 
-use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Client;
 
 class Auth
 {
-    public static function obtainAccessToken() {
+    public static function obtainAccessToken()
+    {
         $client = new Client();
-        $uri = !config('diorgesbb.production') ? 'https://oauth.hm.bb.com.br' : 'https://oauth.bb.com.br';
-        $secrets = !config('diorgesbb.production') ? config('diorgesbb.api.homologa') : config('diorgesbb.api.producao');
+        $uri = ! config('diorgesbb.production') ? 'https://oauth.hm.bb.com.br' : 'https://oauth.bb.com.br';
+        $secrets = ! config('diorgesbb.production') ? config('diorgesbb.api.homologa') : config('diorgesbb.api.producao');
 
         $basic = $secrets['basic'];
-        $formaBasic = base64_encode($secrets['client_id'] . ':' . $secrets['client_secret']);
+        $formaBasic = base64_encode($secrets['client_id'].':'.$secrets['client_secret']);
         if ($basic != $formaBasic) {
             $basic = $formaBasic;
         }
 
         $res = $client->request('POST', $uri.'/oauth/token?grant_type=client_credentials&scope=cobrancas.boletos-info cobrancas.boletos-requisicao', [
             'headers' => [
-                'Authorization' => 'Basic '. $basic,
+                'Authorization' => 'Basic '.$basic,
                 'Accept'     => 'application/json',
 
-            ]
+            ],
         ]);
         $access_token = json_decode($res->getBody()->getContents())->access_token;
 
         return $access_token;
-
     }
 }
--- src/Avalista.php
+++ src/Avalista.php
@@ -17,43 +17,50 @@ class Avalista implements \JsonSerializable
             if (method_exists($this, 'getProtectedFields') && in_array(lcfirst($param), $this->getProtectedFields())) {
                 continue;
             }
-            if (method_exists($this, 'set' . ucwords($param))) {
-                $this->{'set' . ucwords($param)}($value);
+            if (method_exists($this, 'set'.ucwords($param))) {
+                $this->{'set'.ucwords($param)}($value);
             }
         }
     }
 
-    public function setNomeRegistro($nomeRegistro){
+    public function setNomeRegistro($nomeRegistro)
+    {
         $this->nomeRegistro = $nomeRegistro;
     }
 
-    public function setNumeroRegistro($numeroRegistro){
+    public function setNumeroRegistro($numeroRegistro)
+    {
         $this->numeroRegistro = $numeroRegistro;
     }
 
-    public function setTipoRegistro($tipoRegistro){
+    public function setTipoRegistro($tipoRegistro)
+    {
         $this->tipoRegistro = $tipoRegistro;
     }
 
-    public function getNomeRegistro(){
+    public function getNomeRegistro()
+    {
         return $this->nomeRegistro;
     }
 
-    public function getNumeroRegistro(){
+    public function getNumeroRegistro()
+    {
         return $this->numeroRegistro;
     }
 
-    public function getTipoRegistro(){
+    public function getTipoRegistro()
+    {
         return strlen($this->numeroRegistro) <= 11 ? 1 : 2;
     }
 
-    public function jsonSerialize (){
+    public function jsonSerialize()
+    {
         $arr = [];
-        foreach(get_class_methods($this) as $method){
+        foreach (get_class_methods($this) as $method) {
             if (strpos($method, 'get') !== false) {
                 $value = $this->{$method}();
 
-                if(!empty($value) || strlen(trim($value)) > 0){
+                if (! empty($value) || strlen(trim($value)) > 0) {
                     $arr[lcfirst(str_replace(['set', 'get'], ['', ''], $method))] = $value;
                 }
             }
